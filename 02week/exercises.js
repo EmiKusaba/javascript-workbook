a2b90ce4500b50c91aee7c21daf2437f1d16d787
EmiKusaba <eeemi615@gmail.com>
2019-11-05 12:12:04 -0600

    added all of code

--- 02week/exercises.js
+++ 02week/exercises.js
@@ -56,3 +56,34 @@ console.log(removedCars);
 
 var removedCars = (carsInReverse.splice(1, 2,"Ford","Honda"));
 console.log(carsInReverse);
+
+//push
+//Use the push method to add the types of cars that you removed using the splice method to the carsInReverse array.
+carsInReverse.push("Honda","Ford");
+console.log(carsInReverse);
+
+//pop
+//Use the pop method to remove and console.log the last item in the array carsInReverse.
+carsInReverse.pop();
+console.log(carsInReverse);
+
+//shift
+//Use the shift method to remove and console.log the first item in the array carsInReverse.
+carsInReverse.shift();
+console.log(carsInReverse);
+
+//unshift
+//Use the unshift method to add a new type of car to the array carsInReverse.
+carsInReverse.unshift("KIA");
+console.log(carsInReverse);
+
+//forEach
+
+//Create an array called numbers with the following items: 23, 45, 0, 2 Write code that will add 2 to each item in the array numbers.
+//.forEach() requires a function to be passed into it as its first argument. Build a function that will add 2 and then use .forEach() to pass each number into your freshly built function. const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34]
+const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34]
+// var array = [23, 45, 0, 2];
+numbers.forEach(function(element) {
+  console.log(element + 2);
+});
+
