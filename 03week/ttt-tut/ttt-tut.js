5cef5a9b45df608c324c2997cf9dce86dbacb312
EmiKusaba <eeemi615@gmail.com>
2019-11-06 00:31:37 -0600

    added code

--- 03week/ttt-tut/ttt-tut.js
+++ 03week/ttt-tut/ttt-tut.js
@@ -1,1 +1,16 @@
-document.getElementById('coner'),add
+const squares = document.querySelectorAll('board-square');
+squares.forEach(function(item) {
+  item.addEventLisner("click",test);
+});
+
+function handleClick (event){
+  // console.log({target: target.target});
+  let square = event.target;
+  square.textContent = Playturn ;
+  let squareCol = square.dataset.col;
+  let squareRow = square.dataset.row;
+  console.log(squareCol, squareRow);
+  ticTctoe(squareCol, squareRow);
+}
+
+//
