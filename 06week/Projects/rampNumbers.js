f9c787ccf3d6b91b45ce1ced75235b84245c8a30
EmiKusaba <eeemi615@gmail.com>
2019-12-04 20:52:58 -0600

    Add rampNumbers.js

---
+++ 06week/Projects/rampNumbers.js
@@ -0,0 +1,22 @@
+const input = 99999;
+
+function isRamp(num) {
+  const numArray = num.toString().split("");
+  let last = 0;
+  return numArray.every((element) => {
+    const elementNum = Number(element);
+    const isGTE = element >= last;
+    last = element;
+    return isGTE;
+  });
+}
+
+function countRamp(max) {
+  let count = 0;
+  for(let i = 0; i < max; i++) {
+    count += isRamp(i) ? 1 : 0;
+  }
+  return count;
+}
+
+console.log(countRamp(input) + " total ramp numbers less than " + input);
