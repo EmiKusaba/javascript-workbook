117f13025e6849e2a86089d3dc4bb91369411f1d
EmiKusaba <eeemi615@gmail.com>
2021-11-29 15:56:50 -0600

    added js file

--- 03week/TTT part 2/ttt.js
+++ 03week/TTT part 2/ttt.js
@@ -1,107 +1,106 @@
-'use strict';
+//       ***********************
+//            INSTRUCTIONS
+//       ***********************
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// 1. Read the code below and figure out the data flow
+// 2. Add in your code from the terminal app (check for win logic)
+// 3. Look for the @TODO, to fix
+// next to each @TODO you will find tasks that need to be finished
+// 4. GET THIS GAME WORKING!!
 
-// creates and empty "board" for the user to see where marks can be placed.
-// using let because the variable is expected to change with more 'X's and 'O's to add
+let currentMarker = 'X'
 let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
+  ['','',''],
+  ['','',''],
+  ['','','']
 ];
 
-// assigns the first mark as 'X'
-// using let because the variable is expected to change from 'X' to 'O' and back
-let playerTurn = 'X';
-
-// is a function that print the current status of the board using the variable - board
-const printBoard = () => {
-  console.log('   0  1  2');
-  console.log('0 ' + board[0].join(' | '));
-  console.log('  ---------');
-  console.log('1 ' + board[1].join(' | '));
-  console.log('  ---------');
-  console.log('2 ' + board[2].join(' | '));
+// is called when a square is clicked. "this" = element here
+const handleClick = (element) => {
+  // check to see if the square clicked has anything in it, if not continue
+  // this prevents an X being changed to an O
+  if(!document.getElementById(element.id).innerHTML){
+    addMarker(element.id)
+    updateBoard(element.id)
+    checkForWin()
+  }
 }
 
-const horizontalWin = () => {
-  // Your code here to check for horizontal wins
+const addMarker = (id) => {
+  console.log(`We'll place a mark on square: ${id}`)
+  // @TODO, Mix & Match. 
+  // You will need the following pieces:
+  
+  // = currentMarker
+  // .getElementById(id)
+  // document
+  // .innerHTML 
+  
+  // Arrange the above pieces into a single line of code
+  // to add an X or O to the board to the DOM so it can be scene on the screen.
 }
 
-const verticalWin = () => {
-  // Your code here to check for vertical wins
-}
+// passes the element's id attribute from HTML to be used
+const updateBoard = (id) => {
+  // parses the id string into a number then captures the first and last part of the newly created number as row & column
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2)) 
 
-const diagonalWin = () => {
-  // Your code here to check for diagonal wins
+  console.log(`you clicked the sq at ${row} and ${column}`)
+  console.log(board)
+
+  // @TODO, Your code here: use the above information to change the board variable(array of arrays)
+  // HINT: in your browser open up the dev tools -> console
 }
 
 const checkForWin = () => {
-  // Your code here call each of the check for types of wins
+  // calls each checkForWin possibility and if any are true gives a page alert,
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    // **BONUS** you could make the dismissal of this alert window reset the board...
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    // if no win, change the marker from X to O, or O to X for the next player.
+    changeMarker()
+  }
 }
 
-const ticTacToe = (row, column) => {
-  // Your code here to place a marker on the board
-  // then check for a win
+const horizontalWin = () => {
+  // @TODO, Your code here: to check for horizontal wins
 }
 
-const getPrompt = () => {
-  printBoard();
-  console.log("It's Player " + playerTurn + "'s turn.");
-  rl.question('row: ', (row) => {
-    rl.question('column: ', (column) => {
-      ticTacToe(row, column);
-      getPrompt();
-    });
-  });
+const verticalWin = () => {
+  // @TODO, Your code here: to check for vertical wins
 }
 
-
-// Unit Tests
-// You use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
-
-  describe('#ticTacToe()', () => {
-    it('should place mark on the board', () => {
-      ticTacToe(1, 1);
-      assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-    });
-    it('should alternate between players', () => {
-      ticTacToe(0, 0);
-      assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-    });
-    it('should check for vertical wins', () => {
-      board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
-      assert.equal(verticalWin(), true);
-    });
-    it('should check for horizontal wins', () => {
-      board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
-      assert.equal(horizontalWin(), true);
-    });
-    it('should check for diagonal wins', () => {
-      board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
-      assert.equal(diagonalWin(), true);
-    });
-    it('should detect a win', () => {
-      ticTacToe(0, 0)
-      ticTacToe(0, 1)
-      ticTacToe(1, 1)
-      ticTacToe(0, 2)
-      ticTacToe(2, 2)
-      assert.equal(checkForWin(), true);
-    });
-  });
-} else {
-
-  getPrompt();
-
+const diagonalWin = () => {
+  // @TODO, Your code here: to check for diagonal wins
 }
+
+const changeMarker = () => {
+  // ternary operator: if it's an X make it an O, if O make it an X
+  currentMarker = currentMarker === "X" ? "O" : "X"
+}
+
+const resetBoard = () => {
+  // sanity check: this tells us the function is being called
+  console.log("the board was cleared!")
+
+  // collects all of the "td"s into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
+  const squares = document.getElementsByTagName("TD")
+  
+  // loops over the HTML Collections and clears out the Xs and Os
+  for (i=0; i<squares.length; i++) {
+    console.log(squares[i])
+    squares[i].innerHTML = null
+  }
+  
+  // @TODO, Your code here: make sure to reset the array of arrays to empty for a new game
+}
+
+// **BONUSES**
+
+// 1. Display the current player's turn
+// 2. Count number of wins for each player and display them
+// 3. Reset the number of wins
+// 4. Clear the board on alert window dismissal
+// 5. Add players names and display who wins, i.e. "Congrats Emily, you won with 0s!"
