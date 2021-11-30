ca48fcc63815fc7a2f513158d9d990aeabae524f
EmiKusaba <eeemi615@gmail.com>
2021-11-30 07:57:58 -0600

    added win statements

--- 03week/TTT part 2/ttt.js
+++ 03week/TTT part 2/ttt.js
@@ -34,18 +34,47 @@ const printBoard = () => {
 
 const horizontalWin = () => {
   // Your code here to check for horizontal wins
+  if(
+  (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') ||
+  (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') || //or
+  (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') || //or
+  (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O') ||
+  (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O') || //or
+  (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')){
+    return true;
+  }
 }
 
 const verticalWin = () => {
   // Your code here to check for vertical wins
-}
+  if(
+    (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') ||
+    (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') || //or
+    (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') || //or
+    (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') ||
+    (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O') || //or
+    (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')){
+      return true;
+    }
+  }
 
 const diagonalWin = () => {
   // Your code here to check for diagonal wins
-}
+  if(
+    (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
+    (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') || //or
+    (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') ||
+    (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') 
+    ){
+      return true;
+    }
+  }
 
 const checkForWin = () => {
   // Your code here call each of the check for types of wins
+  if (verticalWin() || diagonalWin() || horizontalWin()) {
+    return true;
+  }
 }
 
 const ticTacToe = (row, column) => {
