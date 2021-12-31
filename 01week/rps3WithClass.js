2cd76ac701b6f1ce59ed9529adcfd60f29c4ceb0
EmiKusaba <eeemi615@gmail.com>
2021-12-31 15:55:45 -0600

    added class

--- 01week/rps3WithClass.js
+++ 01week/rps3WithClass.js
@@ -6,70 +6,85 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
 class User {
   constructor() {
-    this.user1 = user1.trim().toLowerCase()
-    this.user2 = user2.trim().toLowerCase()
+    this.wins = 0
+    this.hand = null
+  }
+
+  reset() {
+    this.wins = 0
+    this.hands = null
   }
 }
-const rockPaperScissors = (user1, user2) => {
-// Write code here
 
+class Game {
+  constructor() {
+    this.user1 = new User()
+    this.user2 = new User()
+  }
 
-if(user1 === user2 ){
-  return "It's a tie!";
-} 
-else if (user1 === 'rock' && user2 === 'scissors'){
-  return "user one wins!"
-}
-else if (user1 === 'paper' && user2 === 'rock'){
-  return "user one wins!"
-}
-else if (user1 === 'scissors' && user2 === 'paper'){
-  return "user one wins!"
-}
-else if (user1 === 'scissors' && user2 === 'rock'){
-  return "user two wins!"
-}
-else if (user1 === 'rock' && user2 === 'paper'){
-  return "user two wins!"
-}
-else if (user1 === 'paper' && user2 === 'scissors'){
-  return "user two wins!"
-}
-}
+  start() {
+    this.user1.reset()
+    this.user2.reset()
 
-function getPrompt() {
-  rl.question('user1: ', (answer1) => {
-    rl.question('user2: ', (answer2) => {
-      console.log(rockPaperScissors(answer1, answer2));
-      getPrompt();
+    this.play()
+  }
+
+  play() {
+
+    rl.question('user1 hand: ', (answer1) => {
+
+      this.user1.hand = answer1
+
+      rl.question('user2 hand: ', (answer2) => {
+
+        this.user2.hand = answer2
+
+        const result = this.rockPaperScissors()
+        console.log(`${result} user 1: ${this.user1.wins} user 2: ${this.user2.wins}`)
+
+        this.play()
+      });
     });
-  });
+  }
+
+  rockPaperScissors() {
+
+    const hand1 = this.user1.hand
+    const hand2 = this.user2.hand
+
+    if (hand1 === hand2) {
+      return "It's a tie!";
+    }
+    else if (hand1 === 'rock' && hand2 === 'scissors') {
+      this.user1.wins++
+      return "user one wins!"
+    }
+    else if (hand1 === 'paper' && hand2 === 'rock') {
+      this.user1.wins++
+      return "user one wins!"
+    }
+    else if (hand1 === 'scissors' && hand2 === 'paper') {
+      this.user1.wins++
+      return "user one wins!"
+    }
+    else if (hand1 === 'scissors' && hand2 === 'rock') {
+      this.user2.wins++
+      return "user two wins!"
+    }
+    else if (hand1 === 'rock' && hand2 === 'paper') {
+      this.user2.wins++
+      return "user two wins!"
+    }
+    else if (hand1 === 'paper' && hand2 === 'scissors') {
+      this.user2.wins++
+      return "user two wins!"
+    }
+  }
 }
-//Test
 
-if (typeof describe === 'function') {
+let game = new Game()
+game.start()
 
-describe('#rockPaperScissors()', () => {
-  it('should detect a tie', () => {
-    assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-    assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-    assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-  });
-  it('should detect which hand won', () => {
-    assert.equal(rockPaperScissors('rock', 'paper'), "user two wins!");
-    assert.equal(rockPaperScissors('paper', 'scissors'), "user two wins!");
-    assert.equal(rockPaperScissors('rock', 'scissors'), "user one wins!");
-  });
-  it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-    assert.equal(rockPaperScissors('rOcK', ' paper '), "user two wins!");
-    assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "user two wins!");
-    assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "user one wins!");
-  });
-});
-} else {
-
-getPrompt();
-
-}
